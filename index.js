0d31bd72f5ffc6365a538137260c45c7f47dafdf
dmuthuri <muthuridennis@gmail.com>
2015-12-17 15:36:14 +0300

    add mongodb heroku database endpoints

--- index.js
+++ index.js
@@ -17,7 +17,16 @@ var bodyParser = require('body-parser');
 
 // include mongoose
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/project-aardvark'); 
+var uristring = 
+  process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/project-aardvark';
+
+mongoose.connect(uristring, function(err){
+	if (err) {
+		console.log('Error connecting to: ',uristring);
+	}
+}); 
 
 // express settings
 app.set('port', (process.env.PORT || 8081));
